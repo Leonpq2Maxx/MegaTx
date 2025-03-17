eb92a765272f5eb95c1553a40fd41f5a0f4965d5
MaaxxBrizuela <maxiibriizueelaa15@gmail.com>
2025-03-16 22:34:44 -0300

    Delete prueba 2 directory

--- prueba 2/script.js
+++
@@ -1,9 +0,0 @@
-// Función para cambiar el capítulo automáticamente
-function changeVideo(videoFile) {
-    let videoPlayer = document.getElementById("videoPlayer");
-    let videoSource = document.getElementById("videoSource");
-
-    videoSource.src = videoFile;
-    videoPlayer.load(); // Recargar el nuevo video
-    videoPlayer.play(); // Reproducir automáticamente
-}
